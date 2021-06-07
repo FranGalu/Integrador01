c59e9c41800b902f4dc108e649c34b1c26636caf
Candetallavi <81992016+Candetallavi@users.noreply.github.com>
2021-06-07 18:07:31 -0300

    Add files via upload

---
+++ Integrador01/trabajo-integrador/js/js/generos.js
@@ -0,0 +1,1 @@
+//La información debe provenir de forma dinámica desde la API. Al hacer click sobre un género accederemos a la página de detalle con la información del género clickeado.
---
+++ Integrador01/trabajo-integrador/js/js/playlist.js
@@ -0,0 +1,1 @@
+//La lista de temas debe recuperarse desde el storage del navegador y presentarse al usuario. Cada tema será un link que lleve a su correspondiente página de detalle.
