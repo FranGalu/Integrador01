cfbe409289622c9cdeb42d0b5e6808fb349e4da7
Candetallavi <81992016+Candetallavi@users.noreply.github.com>
2021-06-16 16:08:40 -0300

    Add files via upload

--- Integrador01/trabajo-integrador/js/index.js
+++ Integrador01/trabajo-integrador/js/index.js
@@ -6,9 +6,25 @@
 //Que el campo no esté vacío. Si está vacío avisarle por pantalla al usuario.
 //Que el término buscado tenga al menos 3 caracteres. Si no los tiene avisar por pantalla al usuario
 
+let proxy = 'https://cors-anywhere.herokuapp.com/';
+let canciones = 'https://api.deezer.com/chart/0/tracks';
+let url = proxy + albums; 
+
+fetch(url)
+      .then( function(response){
+          return response.json(); 
+      })
+      .then( function(data){
+          console.log(data);
+          //Nos quedamos s
+      })
+      .catch
+
+
 let mensaje = ""
 
 if (TextoBuscador.value = null);
 mensaje = "Che no escribiste nada en el buscador"
 alert (mensaje)
+
 
