d9c052e44fff97d7e5a708b77102d114aafad903
Franco Galluzzo <francogalluzzo@MacBook-Pro-de-Sebastian.local>
2021-06-18 11:08:59 -0300

    organizacinnn de carpetas

--- Integrador01/trabajo-integrador/js/js/generos.js
+++
@@ -1,1 +0,0 @@
-//La información debe provenir de forma dinámica desde la API. Al hacer click sobre un género accederemos a la página de detalle con la información del género clickeado.
--- Integrador01/trabajo-integrador/js/js/playlist.js
+++
@@ -1,1 +0,0 @@
-//La lista de temas debe recuperarse desde el storage del navegador y presentarse al usuario. Cada tema será un link que lleve a su correspondiente página de detalle.
