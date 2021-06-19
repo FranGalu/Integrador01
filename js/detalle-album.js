2ea443f7fcc1fd4e5cef6bae10db72f9f1981245
Candetallavi <ctallavi@udesa.edu.ar>
2021-06-19 16:15:54 -0300

    ola

--- js/detalle-album.js
+++ js/detalle-album.js
@@ -10,60 +10,14 @@ let album = `https://api.deezer.com/album/${id}`;
 let url = proxy + album;
 
 fetch(url)
-    .then( function(response){
-        return response.json();
-    })
-    .then( function(data){
-        console.log(data);
-        let tracks = data.tracks.data;
-        let lista = document.querySelector('.cancionesdon')
+          .then(function(response){
+              return response.json();
+          })
+         .then(function(data){
+              console.log(data);
 
-        for(let i=0; i<info.length; i++){
-        
-            album +=     `<a href="https://www.youtube.com/watch?v=roSnFm4zsPE"> <img class="don" src"${info[i].picture_medium}" alt="albumes"> </a>
-            <div class="don2" >
-                    <h1 class="tld">"${info[i].title}"</h1> 
-                <div class="datos"> 
-                    <h2> <a class="blanco" href="detalle-artista.html"> Artista: ${info[i].title} </a></h2>
-                    <h2> <a class="blanco"href="detalle-genero.html"> Género: ${info[i].genres.data.name} </a></h2>
-                    <h4>Publicación: ${info[i].release_date}</h4> 
-                </div> 
-            </div>          `          
 
-          for(let i=0; i<tracks.name; i++){
-                   lista.innerHTML += `
-                     <div class="don3">
-                        <div class="dondon">
-                            
-        
-                            <li class="don1">${info[i].tracks.data}</li>
-        
-                          
-                        </div>  
-                        <div class="dondon">
-                            <ol type="a" start="8" class="cancionesdon">
-                     
-                            <li class="don1"></li>
-        
-                            </ol>
-                        </div>
-              
-                    </div>      `
-             
-
-                }  } }   )
-
-    //Nos quedamos solo con el array de datos
-
-        //let image = document.querySelector('img');
-      //  let title = document.querySelector('.tld');
-       // let artista = document.querySelector('.artista');
-        //let año = document.querySelector('.año')
-    // image.src = 
-     //title.innerText = 
-      //  artista.innerText = 
-      //  año.innerText = 
-
-      .catch(function (error){
-        console.log('El error fue: ' + error);
-      })
+         })
+        .catch(function (error){
+            console.log('El error fue: ' + error);
+        })
--- js/detalle-cancion.js
+++ js/detalle-cancion.js
@@ -2,3 +2,57 @@
 //La información de cada página de detalles debe provenir de forma dinámica desde la API. Para acceder a cada página de detalle deberán incorporar query strings en la URL (indicando qué número de artist/album/track) para obtener los datos puntuales desde la API.
 //Cada referencia en las páginas de detalle debe permitir navegación. Ejemplo: si en el detalle de canciones se ve el nombre de un artista al clickear sobre el nombre del artista el sitio debe llevarme al detalle del cantante.
 //Las canciones seleccionadas como favoritas deben guardarse en el storage del navegador. Si la canción fue seleccionada debe indicarse la posibilidad de quitarla de la playlist.
+let queryString = location.search;
+let queryStringOb = new URLSearchParams(queryString);
+let id = queryStringOb.get('id');
+
+let proxy = 'https://cors-anywhere.herokuapp.com/';
+let canciones = `https://api.deezer.com/track/3135556${id}`;
+let url= proxy + canciones; 
+
+fetch (url)
+            .then(function(response){
+                return response.json();
+            })
+            .then(function(data){
+             console.log(data);
+            let lista = document.querySelector('.detalleCancion');
+
+            for(let i=0; i<info.length; i++){
+                detalleCanciones += `<section class="detalleCancion">
+                <img class="dakiti" src="img/el-ultimo-tour.del-mundo.png" alt="Dakiti">
+                <div>
+                <h1 class="daki">"Dakiti"</h1>  
+                  <div class="D"> 
+                     
+                     <a href="playlist.html" class="favorito"> 
+                     <img  class=favorito src="./img/favorito.png" alt=""> </a>
+                     <p class="presione">Presione aquí para agregar</p> 
+                  </div>
+             
+                    <h2> <a class="blanco" href="detalle-artista.html"> Artista: Bad Bunny </a></h2> 
+                    <h2> <a class="blanco" href="detalle-album.html"> Album: "El último tour del mundo" </a></h2>   
+                    <iframe class="video" width="560" height="315" src="https://www.youtube.com/embed/TmKh7lAwnBI" 
+                            title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                            allowfullscreen>
+                    </iframe>
+                
+                </div> 
+        
+        </section>
+        `}
+            
+          //  let imagen = document.querySelector('img');
+          //  let titulo = document.querySelector('.title');
+          //  let artista = document.querySelector('.artist');
+    
+           // image.src = data.cover_medium;
+            //title.innerText = data.title;
+           // artist.innerText = data.artist.name
+
+            })
+
+            .catch(function(error){
+            console.log('El error fue'+ error);
+
+            })
