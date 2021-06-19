86fcda780374d62d21abebb9b2ece49b8d6b7886
Candetallavi <ctallavi@udesa.edu.ar>
2021-06-19 16:40:48 -0300

    volver

--- js/detalle-album.js
+++ js/detalle-album.js
@@ -1,23 +1,3 @@
 //La información de cada página de detalles debe provenir de forma dinámica desde la API. Para acceder a cada página de detalle deberán incorporar query strings en la URL (indicando qué número de artist/album/track) para obtener los datos puntuales desde la API.
 //Cada referencia en las páginas de detalle debe permitir navegación. Ejemplo: si en el detalle de canciones se ve el nombre de un artista al clickear sobre el nombre del artista el sitio debe llevarme al detalle del cantante.
 //Las canciones seleccionadas como favoritas deben guardarse en el storage del navegador. Si la canción fue seleccionada debe indicarse la posibilidad de quitarla de la playlist.
-let queryString = location.search;
-let queryStringToObject = new URLSearchParams(queryString);
-let id = queryStringToObject.get('id');
-
-let proxy = 'https://cors-anywhere.herokuapp.com/';
-let album = `https://api.deezer.com/album/${id}`;
-let url = proxy + album;
-
-fetch(url)
-          .then(function(response){
-              return response.json();
-          })
-         .then(function(data){
-              console.log(data);
-
-
-         })
-        .catch(function (error){
-            console.log('El error fue: ' + error);
-        })
