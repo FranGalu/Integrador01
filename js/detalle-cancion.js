f2ee2e381ba890a87c7cf304d42c0d4064f83e40
Candetallavi <ctallavi@udesa.edu.ar>
2021-06-20 11:03:48 -0300

    dcancion y palylist

--- js/detalle-cancion.js
+++ js/detalle-cancion.js
@@ -7,7 +7,7 @@ let queryStringToObject = new URLSearchParams(queryString);
 let id = queryStringToObject.get('id');
 
 let proxy = 'https://cors-anywhere.herokuapp.com/';
-let dcancion = `https://api.deezer.com/track/3135556${id}`;
+let dcancion = `https://api.deezer.com/track/${id}`;
 let url= proxy + dcancion; 
 
 fetch (url)
@@ -15,7 +15,48 @@ fetch (url)
                 return response.json();
             })
             .then(function(data){
-              console.log(data);  })
+              console.log(data); 
+
+              let title = document.querySelector('.daki');
+              let artist = document.querySelector('.artista');
+              let tapa = document.querySelector('.dakiti');
+              let disco = document.querySelector('.album'); 
+              
+              let canciones = data.canciones.data;
+              let lista = document.querySelector('.detalleCancion');
+
+            
+              title.innerText = data.title;
+              artist.innerText = data.artist.name;
+              tapa.src = data.album.cover_big;
+              disco.innerText = data.album.title;  
+
+              canciones.innerHTML += `  <img class="dakiti" src="img/el-ultimo-tour.del-mundo.png" alt="Dakiti">
+              <div>
+          
+       <h1 class="daki">"Dakiti"</h1>  
+              
+                                       <div class="D"> 
+                   
+                                           <a href="playlist.html" class="favorito"> 
+                                           <img  class=favorito src="./img/favorito.png" alt=""> </a>
+                                            <p class="presione">Presione aquí para agregar</p> 
+                                      </div>
+           
+                  <h2> <a class="artista" href="detalle-artista.html"> Artista: Bad Bunny </a></h2> 
+                  <h2> <a class="album" href="detalle-album.html"> Album: "El último tour del mundo" </a></h2>   
+                  <iframe class="video" width="560" height="315" src="https://www.youtube.com/embed/TmKh7lAwnBI" 
+                          title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                          allowfullscreen>
+                  </iframe>
+              
+              </div> 
+       `
+        
+              
+               
+              
+            })
           
          .catch(function (error){
              console.log('El error fue: ' + error);})
@@ -26,59 +67,6 @@ fetch (url)
 
 
 
-            let seccion  = document.querySelector('.detalleCancion');
-            //  seccion.innerHTML += `<div class="D"> 
-              
-             // <a href="playlist.html" class="favorito"> 
-             // <img  class=favorito src="./img/favorito.png" alt=""> </a>
-             // <p class="presione">Presione aquí para agregar</p> 
-            // </div> `
-            
-            
-            
-              let image = document.querySelector('.dakiti');
-              let title = document.querySelector('.daki');
-              let artist = document.querySelector('.artista');
-              let tapa = document.querySelector('.album');
-            
-            let dcanciones = data.dcanciones.data;
-             let lista = document.querySelector('.detalleCancion');
-            
-              image.src = data.cover_xl;
-              title.innerText = data.title;
-              artist.innerText = data.artist.name;
-              tapa.innerText = data.release_date;
-            
-             // for(let i=0; i<dcanciones.length; i++){
-              
-                    //     lista.innerHTML += `section class="detalleCancion">
-                     //   <img class="dakiti" src="img/el-ultimo-tour.del-mundo.png" alt="Dakiti">
-                      //   <div>
-                     
-                //  <h1 class="daki">"Dakiti"</h1>  
-                         
-                 //          <div class="D"> 
-                              
-                 //             <a href="playlist.html" class="favorito"> 
-                    //          <img  class=favorito src="./img/favorito.png" alt=""> </a>
-                     //         <p class="presione">Presione aquí para agregar</p> 
-                     //      </div>
-                      
-                       //      <h2> <a class="artista" href="detalle-artista.html"> Artista: Bad Bunny </a></h2> 
-                       //      <h2> <a class="album" href="detalle-album.html"> Album: "El último tour del mundo" </a></h2>   
-                       //      <iframe class="video" width="560" height="315" src="https://www.youtube.com/embed/TmKh7lAwnBI" 
-                       //              title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                      //               allowfullscreen>
-                      //       </iframe>
-                         
-                  //       </div> 
-                 
-              //   </section>
-                          // `
-                  
-            
-               //}
-               
             
             
      
