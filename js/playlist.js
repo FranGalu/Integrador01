909962ee9b43ee88b4ec06bfcd536bd512e977a4
Candetallavi <ctallavi@udesa.edu.ar>
2021-06-20 19:41:21 -0300

    playlist y player

--- js/playlist.js
+++ js/playlist.js
@@ -1,1 +1,34 @@
 //La lista de temas debe recuperarse desde el storage del navegador y presentarse al usuario. Cada tema será un link que lleve a su correspondiente página de detalle.
+
+let recuperoStorage = localStorage.getItem('favoritos');
+let favoritos = JSON.parse(recuperoStorage);
+//Donde quiero los datos?
+let otra = document.querySelector('.otra');
+ 
+//Recorrer array
+for (let i=0; i<favoritos.length; i++){
+     buscarYMostrarFavoritos(favoritos[i]);
+}
+
+ function buscarYMostrarFavoritos(id){
+let proxy = 'https://cors-anywhere.herokuapp.com/';
+let playlist = `https://api.deezer.com/track/${id}`; 
+let url = proxy + playlist;
+
+        fetch(url)
+          .then(function(response){
+              return response.json();
+          })
+          .then(function (data){
+            let resultados = '';
+            let info = data.data ; 
+            otra.innerHTML += ` 
+            <p class="tul">${info.title} </p>
+            <a href=""><img class= "imgs" src="${info.picture_medium}" ></a>
+            `
+          } ) 
+          .catch(function (error){
+              console.log(error);
+          })
+        
+        }
