bdac296be2a6e198576fc1788f5db48ba594fedc
benjalanfranco <blanfranco@udesa.edu.ar>
2021-06-23 18:28:15 -0300

    detalle artista

--- js/detalle-artista.js
+++ js/detalle-artista.js
@@ -2,7 +2,6 @@
 //Cada referencia en las páginas de detalle debe permitir navegación. Ejemplo: si en el detalle de canciones se ve el nombre de un artista al clickear sobre el nombre del artista el sitio debe llevarme al detalle del cantante.
 //Las canciones seleccionadas como favoritas deben guardarse en el storage del navegador. Si la canción fue seleccionada debe indicarse la posibilidad de quitarla de la playlist.
 
-//console.log("Hola");
 
 let queryString = location.search;
 let queryStringToObject = new URLSearchParams(queryString);
@@ -10,8 +9,9 @@ let id = queryStringToObject.get('id');
 
 let proxy = 'https://cors-anywhere.herokuapp.com/';
 let artista = `https://api.deezer.com/artist/${id}`;
-let tracklist = `https://api.deezer.com/artist/${id}/top?limit=10`
+let tracklist = `https://api.deezer.com/artist/${id}/albums?limit=5`
 let tracklistCompleta = `https://api.deezer.com/artist/${id}/top?limit=100`
+
 let url = proxy + artista;
 let url2 = proxy + tracklist;
 let url3 = proxy + tracklistCompleta;
@@ -36,7 +36,7 @@ let url3 = proxy + tracklistCompleta;
                     console.log(data);
                         for (let i=0; i<data.data.length; i++){
                           track += `
-                                        <a class="blanco" href="detalle-cancion.html?id=${data.data[i].id}">
+                                        <a class="blanco" href="detalle-album.html?id=${data.data[i].id}">
                                         <li class="sin"> <p> ${data.data[i].title}</p>
                                         </li>  
                                         </a> 
